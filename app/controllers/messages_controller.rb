b4bf69a6e5f7c0c105bc7a4a08530be9b9be096d
Mathis <kmzmathis@gmail.com>
2026-03-08 20:41:28 +0100

    config LLM ok - answer and response - next step SerpAPI tool search news

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,29 @@
 class MessagesController < ApplicationController
+
+  BASE_PROMPT = "Tu prends le rôle d'un oncle cultivé autour d'un diner familial qui répond chaleureusement 
+  et explicitement avec une vraie envie de transmettre de la connaissance."
   def create
+    @chat = current_user.chats.find(params[:chat_id])
+    @message = Message.new(message_params)
+    @message.chat = @chat
+    @message.role = "user"
+    if @message.save
+      llm_set = RubyLLM.chat
+      llm_set_base = llm_set.with_instructions(BASE_PROMPT)
+      llm_prompt = llm_set_base.ask(@message.content)
+      llm_response = llm_prompt.content
+      Message.create(role: "assistant", content: llm_response, chat_id: @chat.id)
+      
+      redirect_to chat_path(@message.chat)
+    else
+      @chat = @message.chat
+      render "chats/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :role, :chat_id)
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,4 @@
 class Message < ApplicationRecord
   belongs_to :chat
+  validates :content, :role, :chat_id, presence: true
 end
