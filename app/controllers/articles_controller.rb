ee92161e960c9928f0845f5815f98138d8edaca4
Mathis <kmzmathis@gmail.com>
2026-03-15 00:27:46 +0100

    implémentation d'un embedding sur la table articles, mais inutile à date. Finalisation de la synthèse de l'actualité par le LLM dans la Show de Resumes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,6 +27,7 @@ class ArticlesController < ApplicationController
         article.save!
       end
     end
+    redirect_to articles_path
   end
 
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,13 @@
 class Article < ApplicationRecord
-  attr_reader :rss_feed_link
+
+  has_neighbors :embedding
+  after_create :set_embedding
+
+  private
+
+  def set_embedding
+    sleep(3)
+    embedding = RubyLLM.embed("rss_title: #{self.rss_title}. rss_description: #{self.rss_desc}. resume_from_llm: #{self.resume_from_llm}")
+    self.update(embedding: embedding.vectors)
+  end
 end
