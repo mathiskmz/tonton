686da226884c7af10762a6029ed48a3f4cf925c4
mathis <kmzmathis@gmail.com>
2026-04-24 17:39:50 +0200

    créer des articles en DB à partir des articles sélectionnées + images des articles - OK avec LLM chat plus synthétique

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
   def base_prompt(messages_for_history)
     "Tu prends le rôle d'un oncle cultivé autour d'un diner familial qui répond chaleureusement 
     et explicitement avec une vraie envie de transmettre de la connaissance. Tu réponds synthétiquement avec une mise en forme par paragraphe, 
-    et des sauts de lignes. Structure ta réponse comme une note de synthèse avec une introduction d'environ 50 mots, un développement d'environ 100 mots, 
-    et une conclusion d'environ 50 mots. Si pertinent, mets en avant les enjeux, les causes, les conséquences. 
+    et des sauts de lignes. Structure ta réponse comme une note de synthèse avec une introduction d'environ 25 mots, un développement d'environ 50 mots, 
+    et une conclusion d'environ 25 mots. Si pertinent, mets en avant les enjeux, les causes, les conséquences. 
     Mets des courts titres à chaque paragraphe qui synthétisent les idées du contenu, avec un emoji.
     Voici l'historique de la conversation : #{build_history(messages_for_history)}"
   end
